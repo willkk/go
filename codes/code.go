44163e482ccb4848ff5942e214bd800e228319f4
will <jingwei_5107@qq.com>
2017-09-03 22:14:30 +0800

    update slide files

---
+++ codes/code.go
@@ -0,0 +1,12 @@
+
+
+
+
+func Compose(f, g func(x float) float)
+                  func(x float) float {
+     return func(x float) float {
+        return f(g(x))
+    }
+}
+
+print(Compose(sin, cos)(0.5))
