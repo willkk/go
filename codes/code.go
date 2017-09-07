bd17e1982447db93e21301b51073884aaecc5904
will <jingwei_5107@qq.com>
2017-09-08 00:05:40 +0800

    update

--- codes/code.go
+++ codes/code.go
@@ -52,3 +52,56 @@ func (f HandlerFunc) ServeHTTP(w ResponseWriter, r *Request) {
     f(w, r)
 }
 // HandlerFunc2 OMIT
+
+
+// Model1 OMIT
+type User struct {
+    Id          int
+    Name        string
+    Profile     *Profile   `orm:"rel(one)"` // OneToOne relation
+    Post        []*Post `orm:"reverse(many)"` // 设置一对多的反向关系
+}
+
+type Profile struct {
+    Id          int
+    Age         int16
+    User        *User   `orm:"reverse(one)"` // 设置一对一反向关系(可选)
+}
+
+type Post struct {
+    Id    int
+    Title string
+    User  *User  `orm:"rel(fk)"`    //设置一对多关系
+    Tags  []*Tag `orm:"rel(m2m)"`
+}
+// Model2 OMIT
+
+o := orm.NewOrm()
+var user User
+user.Name = "slene"
+user.IsActive = true
+
+id, err := o.Insert(&user)
+if err == nil {
+    fmt.Println(id)
+}
+// Model3 OMIT
+o := orm.NewOrm()
+user := User{Id: 1}
+
+err := o.Read(&user)
+// Model4 OMIT
+o := orm.NewOrm()
+user := User{Id: 1}
+if o.Read(&user) != nil {
+    user.Name = "MyName"
+    if num, err := o.Update(&user); err == nil {
+        fmt.Println(num)
+    }
+}
+// Model5 OMIT
+o := orm.NewOrm()
+if num, err := o.Delete(&User{Id: 1}); err == nil {
+    fmt.Println(num)
+}
+// Model6 OMIT
